cd5c4a2204ee1f1ae98fee69b679725b0c34e848
Leiver Moreno <leivermoreno@gmail.com>
2025-09-05 19:26:32 -0400

    add basic implementation of index view

---
+++ coins/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from coins.views import IndexView
+
+
+urlpatterns = [path("", IndexView.as_view(), name="index")]
--- coins/views.py
+++ coins/views.py
@@ -1,3 +1,14 @@
+from pprint import pprint
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from coins.services import get_coin_list
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "coins/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["coin_list"] = get_coin_list()
+        pprint(context["coin_list"])
+        return context
