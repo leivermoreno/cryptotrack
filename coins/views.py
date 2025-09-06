1a62405054ce2b749ec05e2751e50e40c5662ab6
Leiver Moreno <leivermoreno@gmail.com>
2025-09-06 19:59:12 -0400

    add pagination to index

--- coins/views.py
+++ coins/views.py
@@ -1,13 +1,27 @@
-from pprint import pprint
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from coins.services import get_coin_list_with_data
+from coins.services import get_page_count, get_coin_list_with_data
 
 
 class IndexView(TemplateView):
     template_name = "coins/index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["coin_list"] = get_coin_list_with_data()
-        return context
+    def get(self, request):
+        page_count = get_page_count()
+        page = request.GET.get("page", "1")
+        try:
+            page = int(page)
+        except ValueError:
+            page = 1
+        page = min(page, get_page_count())
+        coin_list = get_coin_list_with_data(page)
+
+        return render(
+            request,
+            "coins/index.html",
+            context={
+                "page": page,
+                "page_count": page_count,
+                "coin_list": coin_list,
+            },
+        )
