2b9d6faedb39f01cc380de33d179872c5826c3d9
Leiver Moreno <leivermoreno@gmail.com>
2025-09-06 19:55:55 -0400

    add new service and 2 helpers

--- coins/services.py
+++ coins/services.py
@@ -1,10 +1,13 @@
+import threading
+import math
+
 from django.conf import settings
 import requests
-import threading
 
 
 CG_API_KEY = settings.COINGECKO_KEY
 CG_URL = settings.COINGECKO_ENDPOINT
+RESULTS_PAGE = 100
 
 _thread_local = threading.local()
 
@@ -18,13 +21,31 @@ def _get_session():
     return _thread_local.session
 
 
-def get_coin_list_with_data():
+def get_coin_list():
+
+    res = _get_session().get(
+        CG_URL + "coins/list",
+        params={"status": "active"},
+    )
+
+    return res
+
+
+def get_coin_count():
+    return len(get_coin_list().json())
+
+
+def get_page_count():
+    return math.ceil(get_coin_count() / RESULTS_PAGE)
+
+
+def get_coin_list_with_data(page):
     res = _get_session().get(
         CG_URL + "coins/markets",
         params={
             "vs_currency": "usd",
             "order": "market_cap_desc",
-            "page": "1",
+            "page": page,
             "per_page": 100,
             "price_change_percentage": "24h,7d",
         },
