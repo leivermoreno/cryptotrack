8243a16f170a35916eaf63b79c7ae39ac9c85067
Leiver Moreno <leivermoreno@gmail.com>
2025-09-10 19:04:57 -0400

    add format_amount filter

--- coins/templatetags/coin_extras.py
+++ coins/templatetags/coin_extras.py
@@ -4,6 +4,36 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+def get_decimal_formatted(value, precision, significant_digits):
+    if value == 0:
+        return '0'
+
+    value_str = f'{value:.{precision}f}'.rstrip('0')
+    if value_str == '0.':
+        return '0'
+
+    first_significant_idx = None
+    for idx, digit in enumerate(value_str):
+        if digit not in ('0', '.'):
+            first_significant_idx = idx
+            break
+
+    last_significant_idx = first_significant_idx + significant_digits - 1
+    last_significant_idx = min(last_significant_idx, len(value_str) - 1)
+    return value_str[:last_significant_idx + 1]
+
+@register.filter(is_safe=True)
+def format_amount(value):
+    if value is None:
+        output = '-'
+    elif value == int(value):
+        output = f"{value:,.0f}"
+    elif value >= 1:
+        output = f"{value:,.2f}"
+    else:
+        output = get_decimal_formatted(value, precision=10, significant_digits=4)
+
+    return f'${output}'
 
 @register.simple_tag
 def sort_link(key, page, current_sort, current_direction, content):
