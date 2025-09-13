76c6760e7e89ed96220584559985287d514bfda6
Leiver Moreno <leivermoreno@gmail.com>
2025-09-13 19:04:29 -0400

    add Coin and Watchlist models

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Coin(models.Model):
+    cg_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=50)
+
+
+class Watchlist(models.Model):
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["coin_id", "user_id"], name="unique_watchlist"
+            )
+        ]
