c531602ead069761f93e60f0ed1a18128f25488f
Leiver Moreno <leivermoreno@gmail.com>
2025-09-15 21:35:05 -0400

    refactor render_index to use validated query parameters for pagination and sorting

--- coins/views.py
+++ coins/views.py
@@ -6,35 +6,22 @@ from coins.services import (
     get_page_count,
     get_coin_list_with_data,
     get_simple_coin_data,
-    ALLOWED_SORTS,
-    ALLOWED_DIRECTIONS,
 )
 from coins.models import Watchlist
+from coins.utils import get_validated_query_params
 
 
 def render_index(request):
-    page_count = get_page_count()
-    page = request.GET.get("page", "1")
-    first_page_url = reverse("coins:index")
-    sort = request.GET.get("sort", "rank")
-    direction = request.GET.get("direction", "asc")
     # manual pagination and redirect because data comes from external API
-    redirect_url = None
-    try:
-        page = int(page)
-    except ValueError:
-        page = 0
-    if page < 1:
-        redirect_url = first_page_url
-    elif page > page_count:
-        redirect_url = f"{first_page_url}?page={page_count}"
-    elif sort not in ALLOWED_SORTS or direction not in ALLOWED_DIRECTIONS:
-        redirect_url = reverse("coins:index", query={"page": page})
-
-    if redirect_url:
+    page_count = get_page_count()
+    params = get_validated_query_params(request, page_count)
+    if params["redirect"]:
+        redirect_url = reverse("coins:index") + params["query_string"]
         return redirect(redirect_url)
 
-    page = min(page, get_page_count())
+    page = params["page"]
+    sort = params["sort"]
+    direction = params["direction"]
     coin_list = get_coin_list_with_data(page, sort, direction)
     user_watchlist = (
         list(Watchlist.get_coin_ids_for_user(request.user.id))
