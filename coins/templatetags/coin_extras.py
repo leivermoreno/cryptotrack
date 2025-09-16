e1a34994a1881681983c356ce6ca5dc0b5620e74
Leiver Moreno <leivermoreno@gmail.com>
2025-09-15 21:57:28 -0400

    refactor filters, format_amount now return only - for None values, percentage_change_class now handles None value

--- coins/templatetags/coin_extras.py
+++ coins/templatetags/coin_extras.py
@@ -4,28 +4,27 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-def get_decimal_formatted(value, precision, significant_digits):
-    if value == 0:
-        return '0'
 
-    value_str = f'{value:.{precision}f}'.rstrip('0')
-    if value_str == '0.':
-        return '0'
+def get_decimal_formatted(value, precision, significant_digits):
+    value_str = f"{value:.{precision}f}".rstrip("0")
+    if value_str == "0.":
+        return "0"
 
     first_significant_idx = None
     for idx, digit in enumerate(value_str):
-        if digit not in ('0', '.'):
+        if digit not in ("0", "."):
             first_significant_idx = idx
             break
 
     last_significant_idx = first_significant_idx + significant_digits - 1
     last_significant_idx = min(last_significant_idx, len(value_str) - 1)
-    return value_str[:last_significant_idx + 1]
+    return value_str[: last_significant_idx + 1]
+
 
 @register.filter(is_safe=True)
 def format_amount(value):
     if value is None:
-        output = '-'
+        return "-"
     elif value == int(value):
         output = f"{value:,.0f}"
     elif value >= 1:
@@ -33,14 +32,18 @@ def format_amount(value):
     else:
         output = get_decimal_formatted(value, precision=10, significant_digits=4)
 
-    return f'${output}'
+    return f"${output}"
+
 
 @register.filter(is_safe=True)
 def percentage_change_class(value):
-    if value > 0:
-        return 'text-success'
+    if value is None:
+        return ""
+    if value <= 0:
+        return "text-danger"
     else:
-        return 'text-danger'
+        return "text-success"
+
 
 @register.simple_tag
 def sort_link(key, page, current_sort, current_direction, content):
@@ -53,7 +56,9 @@ def sort_link(key, page, current_sort, current_direction, content):
         else:
             arrow = "&#8595;"
 
-    html_template = "<a class='text-decoration-none' href='?page={}&sort={}&direction={}'>{} {}</a>"
+    html_template = (
+        "<a class='text-decoration-none' href='?page={}&sort={}&direction={}'>{} {}</a>"
+    )
     html = format_html(html_template, page, key, direction, content, mark_safe(arrow))
 
     return html
