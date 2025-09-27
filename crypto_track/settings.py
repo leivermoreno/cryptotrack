220e360d1a2e95891eef26b23e557a7f47a5a447
Leiver Moreno <leivermoreno@gmail.com>
2025-09-27 18:14:51 -0400

    add common app to installed apps

--- crypto_track/settings.py
+++ crypto_track/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "common.apps.CommonConfig",
     "coins.apps.CoinsConfig",
     "accounts.apps.AccountsConfig",
     "crispy_forms",
