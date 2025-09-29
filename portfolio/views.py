f8b11cf897a663d01aa2c21c5e7aa3ec1424fa7e
Leiver Moreno <leivermoreno@gmail.com>
2025-09-29 19:57:54 -0400

    remove unnecessary import from views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import F
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
