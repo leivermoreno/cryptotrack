3134efcf4074ccaf401052ff15e9d42ee76d7009
Leiver Moreno <leivermoreno@gmail.com>
2025-09-29 19:56:04 -0400

    add coin name to allowed sorts and update table header for sorting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,4 +1,4 @@
-ALLOWED_SORTS = ["type", "amount", "price", "total", "created"]
+ALLOWED_SORTS = ["type", "amount", "price", "total", "created", "coin__name"]
 DEFAULT_SORT = "created"
 DEFAULT_DIRECTION = "desc"
 TRANSACTIONS_PER_PAGE = 10
