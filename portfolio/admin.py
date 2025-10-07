4bf6a1352cab9cb86c99f3e19b6ad44580bcdb51
Leiver Moreno <leivermoreno@gmail.com>
2025-10-06 21:01:52 -0400

    add portfolio transaction to the admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from portfolio.models import PortfolioTransaction
+
+
+@admin.register(PortfolioTransaction)
+class PortfolioTransactionAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "coin__name", "type", "amount", "price", "created")
+    list_filter = ("type", "created")
+    search_fields = ("user__username", "coin__name")
+    ordering = ("-created",)
+    fieldsets = (
+        (None, {"fields": ("user", "coin", "type", "amount", "price", "created")}),
+    )
+
+    def has_change_permission(self, request, obj=None):
+        return False
