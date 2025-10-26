0fd53a54fdae4a0cb6bee3ae07329c439a3b8153
Leiver Moreno <leivermoreno@gmail.com>
2025-10-25 22:39:49 -0400

    change default database to use postgresql

--- crypto_track/settings.py
+++ crypto_track/settings.py
@@ -89,8 +89,9 @@ WSGI_APPLICATION = "crypto_track.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crypto_track",
+        "USER": "crypto_track",
     }
 }
 
